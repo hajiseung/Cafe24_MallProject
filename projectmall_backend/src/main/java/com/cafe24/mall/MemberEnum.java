a52a92def79c6a6061d0ea01ab25231f60065167
hajiseung <tmdwlgk0109@naver.com>
2019-07-29 15:44:55 +0900

    Test

--- projectmall_backend/src/main/java/com/cafe24/mall/MemberEnum.java
+++ projectmall_backend/src/main/java/com/cafe24/mall/MemberEnum.java
@@ -3,7 +3,7 @@ package com.cafe24.mall;
 public enum MemberEnum {
 	BRONZE("1"), SILVER("2"), GOLD("3"), PLATINUM("4"), DIAMOND("5");
 	private String name;
-
+ 
 	MemberEnum(String name) {
 		this.name = name;
 	}
